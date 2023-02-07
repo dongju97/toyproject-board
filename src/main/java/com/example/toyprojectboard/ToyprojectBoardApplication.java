4671631ea6a6cc216679804b49bf61ae3ea64b71
seo <fungod19@gmail.com>
2023-02-07 22:57:03 +0900

    #6-스트링부트 프로젝트 시작
    * 스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ src/main/java/com/example/toyprojectboard/ToyprojectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.toyprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyprojectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toyprojectboard/ToyprojectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
