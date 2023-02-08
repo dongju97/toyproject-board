1518e39eed3adbe6db9ea9f4b4ba82550884cf52
dongju97 <97275357+dongju97@users.noreply.github.com>
2023-02-08 12:56:26 +0900

    Revert "Revert "도메인 설계""
    This reverts commit 45a37751855e204c1593ba99f19079aae15dbfdd.

---
+++ src/main/java/com/example/toyprojectboard/domain/Article.java
@@ -0,0 +1,20 @@
+package com.example.toyprojectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title; //제목
+    private String hashtag;
+    private String content;
+
+    private LocalDateTime createAt; //생성일시
+    private String createBy; //생성자
+    private LocalDateTime modifiedAt; //수정일시
+    private String modifiedBy;
+
+
+
+
+
+}
